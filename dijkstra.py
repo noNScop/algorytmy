47b16a993c90814bf2685d765e1f63ee5bd68682
noNScop <srokakarol7@gmail.com>
2023-10-03 12:13:29 +0200

    Skończona podstawowa wersja algorytmu Dijkstry

--- dijkstra.py
+++ dijkstra.py
@@ -1,3 +1,24 @@
+def find_lowest_cost_node(costs):
+    lowest_cost = float("inf")
+    lowest_node = None
+    for node in costs.keys():
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_node = node
+        
+    return lowest_node
+
+
+def print_path(parents, path):
+    if parents:
+        path.append(parents.pop(path[-1]))
+        return print_path(parents, path)
+    else:
+        for i in range(len(path)):
+            print(f"{i + 1}. {path.pop()}")
+
+
 graph = {}
 graph["start"] = {"a": 6, "b": 2}
 graph["a"] = {"meta": 1}
@@ -23,3 +44,7 @@ while node is not None:
             
     processed.append(node)
     node = find_lowest_cost_node(costs)
+
+print(f"Waga najkrótszej drogi: {costs['meta']}")
+print("Droga:")
+print_path(parents, ["meta"])
